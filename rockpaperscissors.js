a16fa96b0456082505ee96dce3e6d95c82fe125d
angieepaynee <aquartullo97@gmail.com>
2023-07-18 11:17:20 -0700

    Change function to only 3 main conditional statements to condense code. getComputerChoice function was spitting out results two times - once before the playRound and once after, thus it was neveer accurate results. Now call the getComputerChoice function after the playRound function and it works.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,12 +1,10 @@
-let playerPrompt = prompt("Would you like to choose rock, paper, or scissors?", " ");
+let playerSelection = prompt("Would you like to choose rock, paper, or scissors?", " ");
 
 
 
-let min = 0;
-let max = 3;
 
 function getComputerChoice() {
-   const gameOptions = (Math.floor(Math.random() * (max-min + 1)) + min);
+   const gameOptions = (Math.floor(Math.random() * 3));
    
    if (gameOptions === 0) {
            return "rock";
@@ -18,29 +16,91 @@ function getComputerChoice() {
    };
 
 
-console.log(getComputerChoice());
-const computerSelection = getComputerChoice();
+// function playRound(playerSelection, computerSelection) {
+
+//    if (playerSelection.toLowerCase() === computerSelection) {
+//       return "It's a tie!";
+//    } 
+   
+//    if (
+//       (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ||
+//       (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+//       (playerSelection.toLowerCase() == "scissors" && computerSelection === "paper")
+//    ) {
+//       return "You win!";
+//     } else if {
+//       return "You lose!";
+//    } 
+   
+//    if (
+//      (computerSelection === "paper" && playerSelection === "rock") ||
+//      (computerSelection === "scissors" && playerSelection === "paper") ||
+//      (computerSelection === "rock" && playerSelection === "scissors") 
+//    ) {
+//       return "You lose!"; 
+//    } else if {
+//       return "You win!";
+//    } 
+// }
+
 
 function playRound(playerSelection, computerSelection) {
 
-   if (
-     (computerSelection === "paper" && playerSelection === "rock") ||
-     (computerSelection === "scissors" && playerSelection === "paper") ||
-     (computerSelection === "rock" && playerSelection === "scissors") 
-    ) {
-      alert("You lose!"); 
-   } else if (
-      (playerSelection === "rock" && computerSelection === "paper") ||
-      (playerSelection === "paper" && computerSelection === "scissors") ||
-      (playerSelection == "scissors" && computerSelection === "rock") 
-   ) {
-      alert("you win!");
-   } else {
-      alert("it's a tie!");
-   }
-}
+   // if (playerSelection.toLowerCase() === computerSelection) {
+   //    return "It's a tie!";
+   // } 
+   // else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+   //    return "you win!";
+   // }
+   // else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+   //    return "you win!";
+   // }
+   // else if (playerSelection.toLowerCase() == "scissors" && computerSelection === "paper") {
+   //    return "you win!";
+   // }
+   // else if (playerSelection === "rock" && computerSelection === "paper") {
+   //    return "You lose!"
+   // }
+   // else if (playerSelection === "paper" && computerSelection === "scissors") {
+   //       return "You lose!"
+   // }
+   // else if (playerSelection === "scissors" && computerSelection === "rock") {
+   //    return "You lose!";
+   // }
 
-const playerSelection = "rock";
+
+
+   if (playerSelection.toLowerCase() === "rock") {
+      if (computerSelection === "paper") {
+         return "You lose!";
+      } else if (computerSelection === "rock") {
+         return "It's a tie!";
+      } else if (computerSelection === "scissors") {
+         return "You win!";
+      }
+   } else if (playerSelection.toLowerCase() === "paper") {
+      if (computerSelection === "paper") {
+         return "It's a tie!";
+      } else if (computerSelection === "rock") {
+         return "You win!";
+      } else if (computerSelection === "scissors") {
+         return "You lose!";
+      }
+   } else if (playerSelection.toLowerCase() === "scissors") {
+      if (computerSelection === "scissors") {
+         return "It's a tie!";
+      } else if (computerSelection === "rock") {
+         return "You lose!";
+      } else if (computerSelection === "paper") {
+         return "You win!";
+      }
+   } };
+// Add conditions for other player selections (scissors, etc.) if needed
+
+
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+
 console.log(playRound(playerSelection, computerSelection));
 
 // let playerScore = 0;
