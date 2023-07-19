890725333fdb7e158ff083a33f4bf74433f53bfb
angieepaynee <aquartullo97@gmail.com>
2023-07-19 09:14:00 -0700

    put all functions within game function. I now have a functioning game.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,70 +1,15 @@
-let playerSelection = prompt("Would you like to choose rock, paper, or scissors? ");   
-let playerScore = 0;
-let computerScore = 0;
-
-
-
-// function getComputerChoice() {
-//    const gameOptions = (Math.floor(Math.random() * 3));
-   
-//    if (gameOptions === 0) {
-//            return "rock";
-//            } else if (gameOptions === 1) {
-//               return "paper";
-//            } else {
-//               return "scissors";
-//            }
-//    };
-
-
-// function playRound(playerSelection, computerSelection) {
-
-//    if (playerSelection === "rock") {
-//       if (computerSelection === "paper") {
-//          computerScore++;
-//          return "You lose!";
-//       } else if (computerSelection === "rock") {
-//          return "It's a tie!";
-//       } else if (computerSelection === "scissors") {
-//          playerScore++;
-//          return "You win!";
-//       }
-//    } else if (playerSelection === "paper") {
-//       if (computerSelection === "paper") {
-//          return "It's a tie!";
-//       } else if (computerSelection === "rock") {
-//          playerScore++;
-//          return "You win!";
-//       } else if (computerSelection === "scissors") {
-//          computerScore++;
-//          return "You lose!";
-//       }
-//    } else if (playerSelection === "scissors") {
-//       if (computerSelection === "scissors") {
-//          return "It's a tie!";
-//       } else if (computerSelection === "rock") {
-//          computerScore++;
-//          return "You lose!";
-//       } else if (computerSelection === "paper") {
-//          playerScore++;
-//          return "You win!";
-//       }
-//    } };
-// console.log(playRound( playerSelection, computerSelection));
-// console.log("Player choice: " + playerSelection.toLowerCase() + "\n" + "Computer choice: " + computerSelection);
-
-
 // initialize initial scores
-
 // update the score when a player wins a round
-
 //when person wins game,add 1 point. when computer wins game, add point.
 //remember to display scores on screen
 //whoever has the most points out of 5, wins. end game.
 //game ends after 5 rounds and display final scores and declare winner
 
-
 function game() {
+   let playerSelection = prompt("Would you like to choose rock, paper, or scissors? ");   
+   let playerScore = 0;
+   let computerScore = 0;
+   
    for(let i = 0; i < 5; i++) {
       let computerSelection = getComputerChoice();
       function getComputerChoice() {
@@ -78,8 +23,7 @@ function game() {
                     return "scissors";
                  }
          };
- 
-      
+
       function playRound(playerSelection, computerSelection) {
       
          if (playerSelection === "rock") {
@@ -113,27 +57,26 @@ function game() {
                return "You win!";
             }
          } };
-         console.log(playRound( playerSelection, computerSelection));
+         console.log(playRound(playerSelection, computerSelection));
          console.log("Player choice: " + playerSelection.toLowerCase() + "\n" + "Computer choice: " + computerSelection);
          console.log("Player Score: " + playerScore + "\n" + "Computer Score: " + computerScore);
    }
+
+   function winner() {
+      if (playerScore > computerScore) {
+         return "You win the game! Yay!";
+      }
+      else if (computerScore > playerScore) {
+         return "Better luck next time";
+      } else {
+         return "Nobody wins!"
+      }
+   } console.log(winner());
 }
 
 game();
 
-// function winner() {
-//    if (playerScore > computerScore) {
-//       return "You win the game";
-//    }
-//    else if (computerScore > playerScore) {
-//       return "better luck next time";
-//    } else {
-//       return "It's a tie!"
-//    }
-// }
 
-// console.log("Player Score: " + playerScore + "\n" + "Computer Score: " + computerScore);
-// console.log(winner(`${playerScore}`, `${computerScore}`));
 
 
 
