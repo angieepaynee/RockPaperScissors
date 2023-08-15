1218d4823fc9ba659d3748783b1ff19e0636c92f
angieepaynee <aquartullo97@gmail.com>
2023-08-15 14:49:59 -0700

    Working code. Displays player/ computer scores and choices, displays the winners of each individual rounds, and displays winner after 5 points. A new game button appears at the end. Starting design now.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,5 @@
-
-      
-
-
  //whole game function
 function game() {
-   let computerSelection = getComputerChoice();
    let playerScore = 0;
    let computerScore = 0;
    let playerSelection = "";
@@ -15,16 +10,11 @@ function game() {
    const winnerElement = document.getElementById('winner');
    const wMessage = document.getElementById('winningMessage');
    const lMessage = document.getElementById('losingMessage');
-   const tMessage = document.getElementById('tieMessage');
-   const tRound = document.getElementById('tieRound');
-   const wRound = document.getElementById('winRound');
-   const lRound = document.getElementById('loseRound');
-   
-   //const buttons = document.querySelectorAll('.mainButtons');
+   const tMessage = document.getElementById('tieMessage');  
+   const buttonContainer = document.getElementById("buttonContainer");
+      
+   let computerSelection = getComputerChoice();
 
-
-
-   //computer selection of rock, paper, or scissors
    function getComputerChoice() {
       const gameOptions = (Math.floor(Math.random() * 3));
       if (gameOptions === 0) {
@@ -35,143 +25,156 @@ function game() {
                   return "scissors";
                }
                };
-   
+
+   function updateScores() {
+      const playerScoreElement = document.getElementById('player-score');
+      const computerScoreElement = document.getElementById('computer-score');
+
+      playerScoreElement.textContent = `Player Score: ${playerScore}`;
+      computerScoreElement.textContent = `Computer Score: ${computerScore}`;
+   };
+
+   function updateChoice() {
+      const playerChoiceElement = document.getElementById('player-choice');
+      const computerChoiceElement = document.getElementById('computer-choice');
+      playerChoiceElement.textContent = `Player Choice: ${playerSelection}`;
+      computerChoiceElement.textContent = `Computer Choice: ${computerSelection}`;
+   };
+
+   function updateDisplay() {
+      computerSelection = getComputerChoice();
+      //call playRound to determine the result of the round
+      //the result variable holds the outcome of the round
+      //update textContent to display results in UI
+      resultsElement.textContent = playRound(playerSelection, computerSelection);
+      winnerElement.textContent = winner();
+      // const winnerResult = winner();
+      // winnerElement.textContent = winnerResult; 
+      document.body.appendChild(resultsElement);
+      document.body.appendChild(choicesElement);
+      document.body.appendChild(scoreElement);
+      document.body.appendChild(winnerElement);
+      updateChoice();
+      updateScores();
+
+   };
          
    //connect buttons to rounds so every time they are clicked a round is played
-      const btnRock = document.getElementById("rock");
-      btnRock.addEventListener('click', () => {
-         playerSelection = "rock";
-         const choice = "Player choice: " + playerSelection + 
-                              " | " + "Computer choice: " + computerSelection;
+   const btnRock = document.getElementById("rock");
+   btnRock.addEventListener('click', () => {
+      playerSelection = "rock";
+      updateDisplay(); 
+   });
 
-         choicesElement.textContent = choice;
-         const score = "Player Score: " + playerScore + " | " + 
-                     "Computer Score: " + computerScore;
-         scoreElement.textContent = score;
-         const result = playRound(playerSelection, computerSelection);
-         resultsElement.textContent = result;
-   
-         const winnerResult = winner();
-         winnerElement.textContent = winnerResult;
+   const btnPaper = document.getElementById("paper");
+   btnPaper.addEventListener('click', () => {
+      playerSelection = "paper";
+      updateDisplay(); 
+      
+   });
+
+   const btnScissors = document.getElementById("scissors");
+   btnScissors.addEventListener('click', () => {
+      playerSelection = "scissors";
+      updateDisplay();
+   });
 
 
-         document.body.appendChild(resultsElement);
-         document.body.appendChild(choicesElement);
-         document.body.appendChild(scoreElement);
-         document.body.appendChild(winnerElement);
-
-         
-      });
-
-      const btnPaper = document.getElementById("paper");
-      btnPaper.addEventListener('click', () => {
-         playerSelection = "paper";
-         const score = "Player Score: " + playerScore + " | " + 
-                       "Computer Score: " + computerScore;
-         scoreElement.textContent = score;
-         const choice = "Player choice: " + playerSelection + 
-                        " | " + "Computer choice: " + computerSelection;
-         choicesElement.textContent = choice;
-         const result = playRound(playerSelection, computerSelection);
-         resultsElement.textContent = result;
-         
-         const winnerResult = winner();
-         winnerElement.textContent = winnerResult;
-
-         document.body.appendChild(resultsElement);
-         document.body.appendChild(choicesElement);
-         document.body.appendChild(scoreElement);
-         document.body.appendChild(winnerElement);
-      });
-
-      const btnScissors = document.getElementById("scissors");
-      btnScissors.addEventListener('click', () => {
-         playerSelection = "scissors";
-         const score = "Player Score: " + playerScore + " | " + 
-                       "Computer Score: " + computerScore;
-         scoreElement.textContent = score;
-
-         const choice = "Player choice: " + playerSelection + 
-                        " | " + "Computer choice: " + computerSelection;
-         choicesElement.textContent = choice;
-         const result = playRound(playerSelection, computerSelection);
-         resultsElement.textContent = result;
-         
-         const winnerResult = winner();
-         winnerElement.textContent = winnerResult;
-
-         document.body.appendChild(resultsElement);
-         document.body.appendChild(choicesElement);
-         document.body.appendChild(scoreElement);
-         document.body.appendChild(winnerElement);
-
-      });
-
-  
-
-   // play a round. Make messages for winning and losing so that i can remove them from the dom when 
-   // I want to display the final winning/losing message
-   function playRound(playerSelection, computerSelection) {
-      computerSelection = getComputerChoice();
+   // play a round.  
+   function playRound(playerSelection) {
 
       if (playerSelection === "rock") {
          if (computerSelection === "paper") {
             computerScore++;
-            return "You lose!";
+            return "You lose this round";
          } else if (computerSelection === "rock") {
             return "It's a tie!";
          } else if (computerSelection === "scissors") {
             playerScore++;
-            return "You win!";
+            return "you win this round!";
          }
       } else if (playerSelection === "paper") {
          if (computerSelection === "paper") {
             return "It's a tie!";
          } else if (computerSelection === "rock") {
             playerScore++;
-            return "You win!";
+            return "you win this round!";
          } else if (computerSelection === "scissors") {
             computerScore++;
-            return "You lose!";
+            return "You lose this round";
          }
       } else if (playerSelection === "scissors") {
          if (computerSelection === "scissors") {
             return "It's a tie!";
          } else if (computerSelection === "rock") {
             computerScore++;
-            return "You lose!";
+            return "You lose this round";
          } else if (computerSelection === "paper") {
             playerScore++;
-            return "You win!";
+            return "you win this round!";
          }
-      } };
+      } 
+      };
 
-   // deciding who is the winner. don't want a return statement since it stops code execution
-   function winner() {
-      if (playerScore > 5) {
-         wMessage.textContent = "You win the game! Yay!";
-      } else if (computerScore > 5) {
-         lMessage.textContent = "You have lost, better luck next time.";
+   function createNewGameButton() {
+      const newGame = document.createElement("button");
+      newGame.textContent = "New Game?";
+      newGame.style.cssText = "background-color: #00BFFF;" +
+      "border: 2px solid black; margin: 0 auto;";
+
+      newGame.addEventListener("click", resetGame);
+      buttonContainer.appendChild(newGame);
+      newGame.addEventListener("click", () => location.reload());
+   };
+
+   function removeNewGameButton() {
+      while (buttonContainer.firstChild) {
+         buttonContainer.removeChild(buttonContainer.firstChild);
+      }  
+   };
+
+   function disableButtons() {
+      btnRock.disabled = true;
+      btnPaper.disabled = true;
+      btnScissors.disabled = true;
+   };
+
+   function updateNewGameButton() {
+      if (playerScore >= 5 || computerScore >= 5) {
+         createNewGameButton();
+
       } else {
+         removeNewGameButton();
+      }
+   };
+
+   // deciding who is the winner. 
+   function winner() {
+      if (playerScore >= 5) {
+         wMessage.textContent = "You win the game! Yay!";
+         disableButtons();
+      } else if (computerScore >= 5) {
+         lMessage.textContent = "You have lost, better luck next time.";
+         disableButtons();
+      } else if (playerScore === 5 && computerScore === 5) {
          tMessage.textContent = "Nobody wins, it's a tie!";
       }
+      updateNewGameButton();
    };
 
    winner();
 
-   //reset game
+
+   // //reset game
    function resetGame() {
       //reset all global variables
-      //remove all event listeners (make a list to loop through them?)
-      //calls the main game function when player wants to start again --- MAKE START BUTTON EVENTUALLY
-      
-      playerScore = 0;
+      //remove all event listeners
       computerScore = 0;
-      scoreElement.removeChild(scores);
-      resultsElement.removeChild(results);
-      choicesElement.removeChild(choices);
-   }
-   
+      playerSelection = "";
+      scoreElement.remove();
+      resultsElement.remove();
+      choicesElement.remove();
+   };
 
 };
 game();    
